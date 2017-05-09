53a47adcf64273ec06f0b802d664ff6982250779
ka4ok85 <makovetsky.evgeny@gmail.com>
2017-05-09 16:46:29 -0400

    switched to spring rest

--- src/main/java/com/example/repository/CallRepository.java
+++ src/main/java/com/example/repository/CallRepository.java
@@ -1,9 +1,18 @@
 package com.example.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.entity.Call;
 
+@RepositoryRestResource
 public interface CallRepository extends JpaRepository<Call, Long> {
+
+	@RestResource (path = "by-type")
+	Collection<Call> findByType(@Param("type") String type);
 
 }
