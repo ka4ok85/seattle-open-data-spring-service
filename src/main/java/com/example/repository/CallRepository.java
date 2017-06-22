10123c5b97e76fab970705a4cbd254255bf2d334
ka4ok85 <makovetsky.evgeny@gmail.com>
2017-06-22 16:44:20 -0400

    "hide" create/update/delete operations for Call Repository

--- src/main/java/com/example/repository/CallRepository.java
+++ src/main/java/com/example/repository/CallRepository.java
@@ -1,8 +1,9 @@
 package com.example.repository;
 
 import java.util.Collection;
+import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -10,9 +11,33 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import com.example.entity.Call;
 
 @RepositoryRestResource
-public interface CallRepository extends JpaRepository<Call, Long> {
+public interface CallRepository extends CrudRepository<Call, Long> {
 
 	@RestResource (path = "by-type")
 	Collection<Call> findByType(@Param("type") String type);
 
+	@Override
+	@RestResource(exported = false)
+	void delete(Long id);
+	
+	@Override
+	@RestResource(exported = false)
+	void delete(Call call);
+	
+	@Override
+	@RestResource(exported = false)
+	void delete(Iterable<? extends Call> entities);
+
+	@Override
+	@RestResource(exported = false)
+	void deleteAll();
+	
+
+    @Override
+    @RestResource(exported = false)
+    public Call save(Call call);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Call> List<S> save(Iterable<S> entities);
 }
