c951918ef9c77dc0ede8e1f89f476322ba671fe7
ka4ok85 <makovetsky.evgeny@gmail.com>
2017-06-29 12:08:11 -0400

    added entity for weather_records table

---
+++ src/main/java/com/example/entity/WeatherRecord.java
@@ -0,0 +1,94 @@
+package com.example.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@Entity
+@Table(name = "weather_records")
+public class WeatherRecord {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "condition_description")
+	private String conditionDescription;
+
+	private String zip;
+
+	@Column(name = "incident_number", unique = true)
+	private String incidentNumber;
+
+	private Float temperature;
+
+	@JsonDeserialize(using = CustomLocalDateTimeDeserializer.class)
+	@JsonSerialize(using = CustomLocalDateTimeSerializer.class)
+	private LocalDateTime datetime;
+
+	public WeatherRecord() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getConditionDescription() {
+		return conditionDescription;
+	}
+
+	public void setConditionDescription(String conditionDescription) {
+		this.conditionDescription = conditionDescription;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getIncidentNumber() {
+		return incidentNumber;
+	}
+
+	public void setIncidentNumber(String incidentNumber) {
+		this.incidentNumber = incidentNumber;
+	}
+
+	public Float getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(Float temperature) {
+		this.temperature = temperature;
+	}
+
+	public LocalDateTime getDatetime() {
+		return datetime;
+	}
+
+	public void setDatetime(LocalDateTime datetime) {
+		this.datetime = datetime;
+	}
+
+	@Override
+	public String toString() {
+		return "WeatherRecord [id=" + id + ", conditionDescription=" + conditionDescription + ", zip=" + zip
+				+ ", incidentNumber=" + incidentNumber + ", temperature=" + temperature + ", datetime=" + datetime
+				+ "]";
+	}
+
+}
