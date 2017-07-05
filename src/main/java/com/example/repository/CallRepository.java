140edf01a9698d1029436561c36bfcdf52294732
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-07-04 22:05:42 -0400

    added query for # of calls per day

--- src/main/java/com/example/repository/CallRepository.java
+++ src/main/java/com/example/repository/CallRepository.java
@@ -1,8 +1,10 @@
 package com.example.repository;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -13,17 +15,20 @@ import com.example.entity.Call;
 @RepositoryRestResource
 public interface CallRepository extends CrudRepository<Call, Long> {
 
-	@RestResource (path = "by-type")
+	@RestResource(path = "by-type")
 	Collection<Call> findByType(@Param("type") String type);
+
+	@Query(value = "select count(v), date(v.datetime) from Call v where v.datetime > :daysBeforeFormatted group by date(v.datetime)")
+	List<?> getCountsDaily(@Param("daysBeforeFormatted") LocalDateTime daysBeforeFormatted);
 
 	@Override
 	@RestResource(exported = false)
 	void delete(Long id);
-	
+
 	@Override
 	@RestResource(exported = false)
 	void delete(Call call);
-	
+
 	@Override
 	@RestResource(exported = false)
 	void delete(Iterable<? extends Call> entities);
@@ -31,13 +36,12 @@ public interface CallRepository extends CrudRepository<Call, Long> {
 	@Override
 	@RestResource(exported = false)
 	void deleteAll();
-	
 
-    @Override
-    @RestResource(exported = false)
-    public Call save(Call call);
+	@Override
+	@RestResource(exported = false)
+	public Call save(Call call);
 
-    @Override
-    @RestResource(exported = false)
-    <S extends Call> List<S> save(Iterable<S> entities);
+	@Override
+	@RestResource(exported = false)
+	<S extends Call> List<S> save(Iterable<S> entities);
 }
