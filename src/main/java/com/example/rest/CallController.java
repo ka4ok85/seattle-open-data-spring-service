d9a7d2f99b602798262a8e3e9d3bba93d6633387
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-07-04 22:09:22 -0400

    added rest endpoint for # of calls per day

---
+++ src/main/java/com/example/rest/CallController.java
@@ -0,0 +1,28 @@
+package com.example.rest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.repository.CallRepository;
+
+@RestController
+@RequestMapping("/calls/")
+public class CallController {
+
+	@Autowired
+	private CallRepository callRepository;
+
+	@RequestMapping(value = "/count/{days}", method = RequestMethod.GET, produces = "application/json")
+	public List<?> getCountsForPeriod(@PathVariable Long days) {
+		LocalDateTime dateTime = LocalDateTime.now().minusDays(days);
+		List<?> countsForPeriod = callRepository.getCountsDaily(dateTime);
+
+		return countsForPeriod;
+	}
+}
