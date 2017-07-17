bfaca66b104c5c6191a03d88d7a2f2bd0e313a86
ka4ok85 <makovetsky.evgeny@gmail.com>
2017-07-17 12:12:28 -0400

    added rest endpoints for date range queries

--- src/main/java/com/example/rest/CallController.java
+++ src/main/java/com/example/rest/CallController.java
@@ -1,6 +1,8 @@
 package com.example.rest;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +21,76 @@ public class CallController {
 	@Autowired
 	private CallRepository callRepository;
 
+	@RequestMapping(value = "/count/{startDate}/{endDate}", method = RequestMethod.GET, produces = "application/json")
+	public List<?> getCountsBetweenDates(@PathVariable String startDate, @PathVariable String endDate,
+			@RequestParam(value = "type", required = false) String type,
+			@RequestParam(value = "zip", required = false) String zip) {
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDateTime startDateTime = LocalDateTime.from(LocalDate.parse(startDate, formatter).atStartOfDay());
+		LocalDateTime endDateTime = LocalDateTime.from(LocalDate.parse(endDate, formatter).atStartOfDay().plusDays(1L)); // we
+
+		List<?> countsForPeriod;
+
+		if (type != null && zip != null) {
+			countsForPeriod = callRepository.getCountsByTypeAndZipDailyBetweenDatetimes(type, zip, startDateTime,
+					endDateTime);
+		} else if (type != null && zip == null) {
+			countsForPeriod = callRepository.getCountsByTypeDailyBetweenDatetimes(type, startDateTime, endDateTime);
+		} else if (type == null && zip != null) {
+			countsForPeriod = callRepository.getCountsByZipDailyBetweenDatetimes(zip, startDateTime, endDateTime);
+		} else {
+			countsForPeriod = callRepository.getCountsDailyBetweenDatetimes(startDateTime, endDateTime);
+		}
+
+		return countsForPeriod;
+	}
+
+	@RequestMapping(value = "/count/per-type/{startDate}/{endDate}", method = RequestMethod.GET, produces = "application/json")
+	public List<?> getCountsPerTypeForPeriod(@PathVariable String startDate, @PathVariable String endDate,
+			@RequestParam(value = "zip", required = false) String zip) {
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDateTime startDateTime = LocalDateTime.from(LocalDate.parse(startDate, formatter).atStartOfDay());
+		LocalDateTime endDateTime = LocalDateTime.from(LocalDate.parse(endDate, formatter).atStartOfDay().plusDays(1L)); // we
+																															// want
+		List<?> countsForPeriod;
+
+		if (zip != null) {
+			countsForPeriod = callRepository.getCountsPerTypeByZipBetweenDatetimes(zip, startDateTime, endDateTime);
+		} else {
+			countsForPeriod = callRepository.getCountsPerTypeBetweenDatetimes(startDateTime, endDateTime);
+		}
+
+		return countsForPeriod;
+	}
+
+	@RequestMapping(value = "/count/per-zip/{startDate}/{endDate}", method = RequestMethod.GET, produces = "application/json")
+	public List<?> getCountsPerZipForPeriod(@PathVariable String startDate, @PathVariable String endDate,
+			@RequestParam(value = "type", required = false) String type) {
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDateTime startDateTime = LocalDateTime.from(LocalDate.parse(startDate, formatter).atStartOfDay());
+		LocalDateTime endDateTime = LocalDateTime.from(LocalDate.parse(endDate, formatter).atStartOfDay().plusDays(1L)); // we
+
+		List<?> countsForPeriod;
+
+		if (type != null) {
+			countsForPeriod = callRepository.getCountsPerZipByTypeBetweenDatetimes(type, startDateTime, endDateTime);
+		} else {
+			countsForPeriod = callRepository.getCountsPerZipBetweenDatetimes(startDateTime, endDateTime);
+		}
+
+		return countsForPeriod;
+	}
+
 	@RequestMapping(value = "/count/{days}", method = RequestMethod.GET, produces = "application/json")
 	public List<?> getCountsForPeriod(@PathVariable Long days,
 			@RequestParam(value = "type", required = false) String type,
 			@RequestParam(value = "zip", required = false) String zip) {
+
 		LocalDateTime dateTime = LocalDateTime.now().minusDays(days);
+
 		List<?> countsForPeriod;
 
 		if (type != null && zip != null) {
@@ -42,8 +109,11 @@ public class CallController {
 	@RequestMapping(value = "/count/per-type/{days}", method = RequestMethod.GET, produces = "application/json")
 	public List<?> getCountsPerTypeForPeriod(@PathVariable Long days,
 			@RequestParam(value = "zip", required = false) String zip) {
+
 		LocalDateTime dateTime = LocalDateTime.now().minusDays(days);
+
 		List<?> countsForPeriod;
+
 		if (zip != null) {
 			countsForPeriod = callRepository.getCountsPerTypeByZipSinceDatetime(zip, dateTime);
 		} else {
@@ -56,7 +126,9 @@ public class CallController {
 	@RequestMapping(value = "/count/per-zip/{days}", method = RequestMethod.GET, produces = "application/json")
 	public List<?> getCountsPerZipForPeriod(@PathVariable Long days,
 			@RequestParam(value = "type", required = false) String type) {
+
 		LocalDateTime dateTime = LocalDateTime.now().minusDays(days);
+
 		List<?> countsForPeriod;
 
 		if (type != null) {
